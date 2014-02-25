fe3415c3c0a9587318d3a5ca32b73543bf1ed756
Magnus Hanses <hanses.magnus@gmail.com>
2014-02-25 22:25:17 +0100

    added install script and cleaned up

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# remove existing links
+rm ~/.bashrc
+rm ~/.gitconfig
+rm ~/.config/terminator/config
+rm ~/.tmux.conf
+rm ~/.tmuxline
+rm ~/.vimconfig
+rm ~/.vimrc
+rm ~/.Xmodmap
+rm ~/.zshrc
+rm ~/.zshrc.local
+
+# create links
+ln -s ~/github/dotfiles/_bashrc ~/.bashrc
+ln -s ~/github/dotfiles/_gitconfig ~/.gitconfig
+ln -s ~/github/dotfiles/_terminatorconf ~/.config/terminator/config
+ln -s ~/github/dotfiles/_tmux.conf ~/.tmux.conf
+ln -s ~/github/dotfiles/_tmuxline ~/.tmuxline
+ln -s ~/github/dotfiles/_vimconfig ~/.vimconfig
+ln -s ~/github/dotfiles/_vimrc ~/.vimrc
+ln -s ~/github/dotfiles/_Xmodmap ~/.Xmodmap
+ln -s ~/github/dotfiles/_zshrc ~/.zshrc
+ln -s ~/github/dotfiles/_zshrc.local ~/.zshrc.local
+

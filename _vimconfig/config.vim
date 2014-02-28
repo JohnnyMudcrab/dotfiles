19f174039a9a9a05d6dba2f15e3355cebaf00495
Magnus Hanses <hanses.magnus@gmail.com>
2014-02-28 19:46:50 +0100

    added tmuxinator

---
+++ _vimconfig/config.vim
@@ -0,0 +1,6 @@
+source ~/.vimrc
+e ~/.vimconfig/vundle.vim
+tabnew ~/.vimconfig/plugins.vim
+tabnew ~/.vimconfig/complete.vim
+tabnew ~/.vimconfig/mappings.vim
+
--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ rm ~/.gitconfig
 rm ~/.config/terminator/config
 rm ~/.tmux.conf
 rm ~/.tmuxline
+rm -rf ~/.tmuxinator
 rm ~/.vimconfig
 rm ~/.vimrc
 rm ~/.Xmodmap
@@ -18,6 +19,7 @@ ln -s ~/github/dotfiles/_gitconfig ~/.gitconfig
 ln -s ~/github/dotfiles/_terminatorconf ~/.config/terminator/config
 ln -s ~/github/dotfiles/_tmux.conf ~/.tmux.conf
 ln -s ~/github/dotfiles/_tmuxline ~/.tmuxline
+ln -s ~/github/dotfiles/_tmuxinator ~/.tmuxinator
 ln -s ~/github/dotfiles/_vimconfig ~/.vimconfig
 ln -s ~/github/dotfiles/_vimrc ~/.vimrc
 ln -s ~/github/dotfiles/_Xmodmap ~/.Xmodmap

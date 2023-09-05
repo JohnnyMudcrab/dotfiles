958083fa1d34cfb8ceae0708eb23abccc26ac0f3
Magnus Hanses <magnus.hanses@iff.fraunhofer.de>
2023-09-05 23:00:13 +0200

    updated config

--- lazyvim/lua/plugins/dap.lua
+++ lazyvim/lua/plugins/dap.lua
@@ -1,5 +1,10 @@
 return {
     "jay-babu/mason-nvim-dap.nvim",
+    keys = function()
+        return {
+            { "<leader>cR", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+        }
+    end,
     opts = function(_, opts)
         opts.ensure_installed = { "cpptools" }
     end,
---
+++ lazyvim/lua/plugins/telescope.lua
@@ -0,0 +1,6 @@
+return {
+    "nvim-telescope/telescope.nvim",
+    opts = function(_, opts)
+        opts.defaults.path_display = { "truncate" }
+    end,
+}

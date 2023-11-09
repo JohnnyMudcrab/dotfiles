bc4560d496fbdb1cef58d1230fdd81730945b97b
Magnus Hanses <magnus.hanses@iff.fraunhofer.de>
2023-11-09 14:50:17 +0100

    fixed launch.lua

--- lazyvim/lua/plugins/launch.lua
+++ lazyvim/lua/plugins/launch.lua
@@ -2,7 +2,8 @@ return {
     "dasupradyumna/launch.nvim",
     -- add below plugins as per user requirement
     dependencies = {
-        'mfussenegger/nvim-dap',
-        'rcarriga/nvim-notify',
-    }
+        "mfussenegger/nvim-dap",
+        "rcarriga/nvim-notify",
+    },
+    opts = {},
 }

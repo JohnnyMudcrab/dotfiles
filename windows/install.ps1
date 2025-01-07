c7decebc6d3acbffd694046bc749626f9224d6ae
Hanses <hanses@iff.intern>
2025-01-07 09:25:00 +0100

    added install script for windows

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,5 +1,5 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
-irm get.scoop.sh | iex
+irm get.scoop.sh | iex #TODO: check if already installed
 scoop bucket add extras
-scoop install nvim neovide gcc python lazygit
+scoop install neovim neovide gcc python lazygit
 # TODO: make symlinks for nvim config under windows

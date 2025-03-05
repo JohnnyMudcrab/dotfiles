22d58fd3116b38d175beb216f7a45e0ef8b564f2
Magnus <magnus.hanses@iff.fraunhofer.de>
2025-03-05 16:19:36 +0100

    update

--- lazyvim/lua/plugins/navbuddy.lua
+++ lazyvim/lua/plugins/navbuddy.lua
@@ -1,19 +1,19 @@
 return {
-    {
-        "SmiteshP/nvim-navbuddy",
-        keys = {
-            {
-                "<leader>j",
-                function()
-                    require("nvim-navbuddy").open()
-                end,
-                desc = "Navbuddy",
-            },
-        },
-        opts = {
-            lsp = {
-                auto_attach = true,
-            },
-        },
-    },
+    -- {
+    --     "SmiteshP/nvim-navbuddy",
+    --     keys = {
+    --         {
+    --             "<leader>j",
+    --             function()
+    --                 require("nvim-navbuddy").open()
+    --             end,
+    --             desc = "Navbuddy",
+    --         },
+    --     },
+    --     opts = {
+    --         lsp = {
+    --             auto_attach = true,
+    --         },
+    --     },
+    -- },
 }

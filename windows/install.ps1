b2a95b258d9a37f3a3c5cb617caf4c5ec036a5a1
Hanses <hanses@iff.intern>
2025-04-09 14:25:27 +0200

    update

--- windows/install.ps1
+++ windows/install.ps1
@@ -1,5 +1,18 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex #TODO: check if already installed
+scoop install git
 scoop bucket add extras
-scoop install neovim neovide gcc python lazygit
+scoop install neovim neovide gcc python lazygit wezterm
+scoop install autohotkey
+scoop install vcredist2022
+scoop install powertoys
+scoop install onecommander
+scoop install brave
+scoop install blender
+scoop install xmind
+scoop install inkscape
+scoop install zotero
+scoop install yed
+
+# TODO set Home Path
 # TODO: make symlinks for nvim config under windows
